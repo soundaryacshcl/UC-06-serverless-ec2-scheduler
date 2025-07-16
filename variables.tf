1169d0fd634002cc170efb30df1fb420e9d9f4d1
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-16 22:22:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ec2_instance_ids" {
+  description = "EC2 instance IDs"
+  type        = string
+  default     = "i-080fbb2b0a5d6e3fd"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
