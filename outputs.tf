a7b505b1bcd906ddaab4e16bf5e88df76c17fc4b
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-16 22:21:07 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "start_lambda_function_name" {
+  value = module.lambda_start.this.function_name
+}
+
+output "stop_lambda_function_name" {
+  value = module.lambda_stop.this.function_name
+}
+
+output "start_event_rule" {
+  value = module.start_schedule.event_rule_name
+}
+
+output "stop_event_rule" {
+  value = module.stop_schedule.event_rule_name
+}
